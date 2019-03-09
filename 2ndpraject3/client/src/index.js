dec4b87f7fed411f4ce1bc5abc7d364bcfbffefe
Teddy <koolaidbelly@gmail.com>
2019-03-09 13:34:52 -0500

    react routes to pages

--- 2ndpraject3/client/src/index.js
+++ 2ndpraject3/client/src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import UserInput from "./pages/UserInput";
+import Search from "./pages/Search";
+import Review from "./pages/Review";
 import App from './App';
 
+const routing = (
+    <Router>
+        <div>
+            <ul>
+                <li>
+                <Link to="/user">User </Link>
+                </li>
+                <li>
+                <Link to="/review">Review</Link>
+                </li>
+                <li>
+                <Link to="/search">Contact</Link>
+                </li>
+            </ul>
+            <Route exact path="/" component={App}/>
+            <Route path="/user" component={UserInput}/>
+            <Route path="/search" component={Search}/>
+            <Route path="/review" component={Review}/>
+        </div>
+    </Router>
+)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
