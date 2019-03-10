681b98c022fad1a457769697cbd5dc592a904d49
Teddy Johnson <koolaidbelly@gmail.com>
2019-03-10 04:55:50 -0400

    Update App.js & UserInput.js

--- 2ndpraject3/client/src/App.js
+++ 2ndpraject3/client/src/App.js
@@ -1,16 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import Search from "./pages/Search";
+import Review from "./pages/Review";
+import User from './pages/UserInput';
 // import logo from './logo.svg';
 
 
-class App extends Component {
-  
-  render() {
-    return (
-      <div>
-        
-      </div>
-    );
-  }
+ function App() {
+return(
+    <Router>
+        <div>
+            <ul>
+                <li>
+                <Link to="/user">User </Link>
+                </li>
+                <li>
+                <Link to="/review">Review</Link>
+                </li>
+                <li>
+                <Link to="/search">Contact</Link>
+                </li>
+            </ul>
+            <Switch>
+              <Route exact path="/" component={User}/>
+              <Route path="/user" component={User}/>
+              <Route path="/search" component={Search}/>
+              <Route path="/review" component={Review}/>
+            </Switch>
+        </div>
+    </Router>
+)
 }
 
 export default App;
--- 2ndpraject3/client/src/pages/UserInput.js
+++ 2ndpraject3/client/src/pages/UserInput.js
@@ -1,31 +1,84 @@
 import React from 'react'
 
 class UserInput extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            FirstName: '',
-            LastName:'',
-            Address:'',
-            City:'',
-            State:''
-            }
-        this.handleChange = this.handleChange.bind(this)
+  constructor(props){
+    super(props);
+    this.state = {
+      nameFirst: '',
+      nameLast:'',
+      nameAddress:'',
+      nameCity:'',
+      nameState:'',
+      nameZipcode:'',
+      nameEmail:'',
       }
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const {name,value}  = event.target;
+    this.setState({[name]: value})
+  }
+  handleSubmit(event){
+    event.preventDefault();
+  }
     
-      handleChange(event) {
-        this.setState({ value: event.target.value })
-      }
   render() {
+  
     return( <div>
         <h1>Users</h1>
         <form>
-        <input
+        <input 
           type="text"
-          value={this.state.username}
+          name="nameFirst"
+          value={this.state.nameFirst}
           onChange={this.handleChange}
         />
+        <input
+          type="text"
+          name="nameLast"
+          value={this.state.nameLast}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="nameAddress"
+          value={this.state.nameAddress}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="nameCity"
+          value={this.state.nameCity}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="nameState"
+          value={this.state.nameState}
+          onChange={this.handleChange}
+        />
+        <input
+          type="number"
+          name="nameZipcode"
+          value={this.state.nameZipcode}
+          onChange={this.handleChange}
+        />
+        <input
+          type="email"
+          name="nameEmail"
+          value={this.state.nameEmail}
+          onChange={this.handleChange}
+        />
+        <input type="submit" value="Submit" onClick={this} />
         </form>
+
+        <h2>{this.state.nameFirst}</h2>
+        <h2>{this.state.nameLast}</h2>
+        <h2>{this.state.nameZipcode}</h2>
+        <h2>{this.state.nameEmail}</h2>
+        <h2>{this.state.nameFirst}</h2>
         </div>)
   }
 }
