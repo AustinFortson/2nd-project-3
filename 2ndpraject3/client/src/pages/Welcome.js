6b700cfab2dff978e9bba5472115714f942c337b
Austin <a.rossfortson@gmail.com>
2019-03-21 17:53:49 -0400

    Everything I have

--- 2ndpraject3/client/src/pages/Welcome.js
+++ 2ndpraject3/client/src/pages/Welcome.js
@@ -13,11 +13,13 @@ class Welcome extends React.Component {
    return(
 
        <div id="welcomeBG">
-
+       <div id="getStart">
        <img id="welcomeH" src="https://corporate.homedepot.com/sites/default/files/styles/hero/public/image_gallery/Homer_Header.png?itok=YmsccXh1" ></img>
 
-       <div id="getStart">
+
        <h1>Welcome to the Home Depot Tool Rental Application</h1>
+
+       <a class="waves-effect waves-light btn-small" id="getStarted"><i class="material-icons left">build</i>Get Started</a>
        </div>
 
        </div>
